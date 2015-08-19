437ddf4a03100419fe1d0606f6c7f0f050ea937d
Serge Lerner <sergel@Sergei-Pc.netcraft.local>
2015-08-19 17:57:07 +0300

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+var React    = require('react');
+var MainComp = require('./components/main');
+
+React.render(<MainComp/>, document.getElementById("app"));
---
+++ app/scripts/components/main.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = React.createClass({
+
+  render: function() {
+
+	return (
+		<div>		
+			<h1>Yoo</h1>	
+		</div>
+	);
+  }
+});
+
+module.exports = Main;
